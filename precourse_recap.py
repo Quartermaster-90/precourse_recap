b9617005d3da5e93998697ca7f5da9eb9ab9770a
Quartermaster-90 <christopher.belsey@icloud.com>
2023-05-29 22:37:36 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,13 @@
+year = 1990
+
+if year > 2020
+    print("You're in your third decade")
+else:
+    print("You're still young :-)")
+
+def how_old(year, DOB)
+    print("Year of birth " + str(DOB) - str(year) = result)
+
+year = 1990
+DOB = 1993
+how_old(year, DOB)
