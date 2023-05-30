cf385cedfaf415dfeb7bf366d7901c2356da2ce0
Quartermaster-90 <christopher.belsey@icloud.com>
2023-05-30 07:45:23 +0100

    modified code

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,13 +1,12 @@
 year = 1990
 
-if year > 2020
+if year > 2020:
     print("You're in your third decade")
 else:
     print("You're still young :-)")
 
-def how_old(year, DOB)
-    print("Year of birth " + str(DOB) - str(year) = result)
+def how_old(year, DOB):
 
-year = 1990
+    year = 1990
 DOB = 1993
 how_old(year, DOB)
